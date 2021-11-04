a31a2bbe35528ca7c631420155b0ce859895c160
amaclean199 <maclean199@gmail.com>
2021-11-04 09:01:25 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('aframe');
+
+aframe.registerComponent("audiohandler", {
+  schema: {
+    id: { default: "none" }
+  },
+
+  init: function() {
+    var data = this.data;
+    let playing = false;
+    let audiosource = document.querySelector(data.id);
+
+    this.el.addEventListener("click", () => {
+      if (!playing) {
+        audiosource.components.sound.playSound();
+      } else {
+        audiosource.components.sound.stopSound();
+      }
+
+      playing = !playing;
+      console.log("click event");
+    });
+  }
+});
