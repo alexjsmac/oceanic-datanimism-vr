a5af1300530914917de8cd029b5dbbdd922725b0
amaclean199 <maclean199@gmail.com>
2021-12-06 20:08:29 -0500

    Add kick drum part

--- js/index.js
+++ js/index.js
@@ -11,12 +11,12 @@ Papa.parse('https://raw.githubusercontent.com/datasets/sea-level-rise/master/dat
     let bar = 0;
     for (let i = 0; i < data.length-1; i++) {
       let note = Math.round(data[i]["CSIRO Adjusted Sea Level"]) + 1;
-      let beat = i % 4;
+      let beat = (i % 2) * 2;
       if (i != 0 && beat == 0){
         bar += 1;
       }
       time = bar.toString() + ":" + beat.toString();
-      mainMelody.push({'time': time, 'note': notes[note], 'duration': '8n'});
+      mainMelody.push({'time': time, 'note': notes[note], 'duration': '4n'});
     }
   }
 });
@@ -29,6 +29,47 @@ const synth2 = new Tone.Synth({
     type : "fatsawtooth"
   }
 }).toDestination();
+
+const kickDrum = new Tone.MembraneSynth({
+  volume: 6
+}).toDestination();
+
+const kicks = [
+  { time: '0:0' },
+  { time: '0:3:2' },
+  { time: '1:1' },
+  { time: '2:0' },
+  { time: '2:1:2' },
+  { time: '2:3:2' },
+  { time: '3:0:2' },
+  { time: '3:1:' },
+  { time: '4:0' },
+  { time: '4:3:2' },
+  { time: '5:1' },
+  { time: '6:0' },
+  { time: '6:1:2' },
+  { time: '6:3:2' },
+  { time: '7:0:2' },
+  { time: '7:1:' },
+];
+
+let fullKicks = Array();
+for (let i = 0; i < 8; i++) {
+  kicks.forEach(element => {
+    let time = element.time;
+    let timeNumbers = time.split(":")
+    let bar = parseInt(timeNumbers[0]);
+    bar = bar + 8 * i;
+    timeNumbers = timeNumbers.slice(1);
+    time = bar + ":" + timeNumbers.join(":");
+    fullKicks.push({'time': time});
+  })
+}
+console.log(fullKicks);
+
+const kickPart = new Tone.Part(function(time){
+  kickDrum.triggerAttackRelease('C1', '8n', time)
+}, fullKicks).start(0);
 
 AFRAME.registerComponent("audiohandler", {
   schema: {
