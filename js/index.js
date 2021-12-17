acb28a8816491b9d029e427237bd36a201e5c9f6
amaclean199 <maclean199@gmail.com>
2021-12-17 11:07:28 -0500

    Add back text

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
 AFRAME.registerComponent("audiohandler", {
+
   schema: {
     id: { default: "none" },
   },
+
   init: function () {
     this.el.addEventListener("click", async () => {
+
       generateElements();
       alterEveryOtherPath();
 
       if (Tone.Transport.state !== "started") {
+
         console.log("Starting playback");
-        let text = document.querySelector("#begin-text");
+        let text = document.querySelector("#mainText");
+
         const mainMelodyPart = new Tone.Part(function (time, note) {
           console.log(note);
           synth.triggerAttackRelease(note.note, note.duration, time);
           text.setAttribute("text-geometry", "value: " + note.year);
         }, mainMelody).start(0);
+
         Tone.start();
         Tone.Transport.start();
+
       } else {
         console.log("Stopping playback");
         Tone.Transport.stop();
